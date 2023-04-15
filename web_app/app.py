0e42682bed31521886fc9f317620beae15db02ea
pvulya1 <eugene.paulya@gmail.com>
2023-04-15 11:50:39 -0600

    changed search to autocomplete and dropdown

--- web_app/app.py
+++ web_app/app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request, jsonify
 import pandas as pd
 import numpy as np
 import pickle
@@ -10,7 +10,12 @@ with open("wine_recommender.pkl", "rb") as f:
     
 def recommend_wines_by_title(wine_title, n_recommendations=10):
     # Find the index of the wine with the given title
-    wine_index = titles[titles == wine_title].index[0]
+    wine_index = titles[titles == wine_title].index
+    
+    if wine_index.empty:
+        return ["Your input hasn't been found in the dataset!"]
+    
+    wine_index = wine_index[0]
     
     # Get similarity scores for the given wine
     similarity_scores = list(enumerate(similarity_matrix[wine_index]))
@@ -23,6 +28,15 @@ def recommend_wines_by_title(wine_title, n_recommendations=10):
     
     # Return the titles of the most similar wines
     return titles.iloc[most_similar_indices]
+
+@app.route('/wine_titles', methods=['GET'])
+def wine_titles():
+    term = request.args.get('term', '')
+    print(f"Search term: {term}")  # Add this line to print the search term
+    suggestions = titles[titles.apply(lambda x: term.lower() in x.lower())].unique().tolist()
+    print(f"Suggestions: {suggestions}")  # Add this line to print the suggestions
+    return jsonify(suggestions)
+
 
 @app.route("/", methods=["GET", "POST"])
 def index():
